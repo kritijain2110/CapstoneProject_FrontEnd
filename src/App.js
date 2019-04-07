7dde90fa56f436ec81b9193fcdccedc02d89bb03
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-04-07 22:50:40 +0530

    Pushed all newly added files to git

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './screens/home/Home';
+import Header from './common/header/Header';
+import Profile from './screens/profile/Profile';
+import Details from './screens/details/Details';
+import Checkout from './screens/checkout/Checkout';
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+
+                    <Switch>
+                        <Route path='/' exact={true} component={Header} />
+                        <Route path='/Home' exact={true} component={Home} />
+                        <Route path='/Profile' exact={true} component={Home} />
+                        <Route path='/Detail' exact={true} component={Details} />
+                        <Route path='/Checkout' exact={true} component={Checkout} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ src/common/header/Header.js
@@ -0,0 +1,237 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import logo from '../../assets/logo.SVG';
+import Button from '@material-ui/core/Button';
+import Basic from './Basic.js';
+
+
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router-dom';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        backgroundColor:'#263238',
+    },
+    shape:
+        {
+            InputBaseBoaderRadius:'4px',
+        },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    appbar:
+        {
+            backgroundColor:"#263238",
+        },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: '4px',
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing.unit * 2,
+        marginLeft: '400px',
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit * 3,
+            width: '300px',
+
+        },
+    },
+    searchBox:
+        {
+            marginLeft:'400px',
+            borderRadius:'4px',
+        },
+
+    searchIcon: {
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        borderRadius:'4px',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '300',
+
+            backgroundColor:'#c0c0c0',
+            borderradius: '4px',
+        },
+    },
+    
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+});
+
+class Header extends React.Component {
+    state = {
+        anchorEl: null,
+        mobileMoreAnchorEl: null,
+    };
+
+    handleProfileMenuOpen = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleMenuClose = () => {
+        this.setState({ anchorEl: null });
+        this.handleMobileMenuClose();
+    };
+
+    handleMobileMenuOpen = event => {
+        this.setState({ mobileMoreAnchorEl: event.currentTarget });
+    };
+
+    handleMobileMenuClose = () => {
+        this.setState({ mobileMoreAnchorEl: null });
+    };
+
+    render() {
+        const { anchorEl, mobileMoreAnchorEl } = this.state;
+        const { classes,screen } = this.props;
+        const isMenuOpen = Boolean(anchorEl);
+        const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+        const renderMenu = (
+            <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={isMenuOpen}
+                onClose={this.handleMenuClose}
+            >
+
+
+                <div>
+
+                    <Link to={"/profile"}><MenuItem onClick={this.handleMenuClose }>
+                        My Profile</MenuItem></Link>
+                </div>
+
+                <hr/>
+                <Link to={"/"}><MenuItem onClick={this.handleMenuClose}>Log Out</MenuItem></Link>
+            </Menu>
+        );
+
+        const renderMobileMenu = (
+            <Menu
+                anchorEl={mobileMoreAnchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={isMobileMenuOpen}
+                onClose={this.handleMenuClose}
+            >
+
+
+                <MenuItem onClick={this.handleProfileMenuOpen}>
+                    <IconButton color="inherit">
+                        <AccountCircle />
+
+                    </IconButton>
+                    <p>Profile</p>
+                </MenuItem>
+            </Menu>
+        );
+
+        return (
+            <div className={classes.root}>
+                <AppBar className={classes.appbar}position="static" backgroundColor="#263238">
+                    <Toolbar>
+                    <img src={logo} className="app-logo" alt="Movies App Logo" />
+                        <Toolbar>
+                            {(screen === "Home")}
+                            <div className={classes.searchBox}>
+                                <div className={classes.search}>
+                                    <div className={classes.searchIcon}>
+                                        <SearchIcon />
+
+                                    </div>
+                                    <div className={classes.searchTemp} >
+                                        <InputBase
+                                        //onChange={this.props.searchResurantByDescription.bind(this)}
+                                            placeholder="Search by Resturant Name"
+                                            enableUnderline
+                                            classes={{
+                                                root: classes.inputRoot,
+                                                input: classes.inputInput,
+                                            }}
+                                        />
+                                    </div>
+                               </div>
+
+                            </div>
+
+                        </Toolbar>
+                        <div className={classes.grow} />
+                        <div className={classes.sectionDesktop}>
+                            <Button  variant="contained" color="default"  onClick={this.handleProfileMenuOpen}>
+                             <AccountCircle /> Upgrad
+                              </Button>
+                        </div>
+                           <Basic/>
+                        <div className={classes.sectionMobile}>
+                            <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+                                <MoreIcon />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                {renderMenu}
+                {renderMobileMenu}
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
